c069cef2bf618fdbcf26cf47e2caeb61c22d1c56
Mounst <jose.hernandez19@sistemas.tecsanpedro.edu.mx>
2024-10-10 22:28:40 -0600

    Elaboramos la descripcion de el programa con su mensaje de bienvenida

---
+++ Actualizador.py
@@ -0,0 +1,18 @@
+descripcion = "Este programa funciona como un actualizador de perfil"
+autor = "Jose Hernandez Favela"
+fecha = "Ultima modificacion: 10/10/2024"
+licencia = "Licencia MIT"
+pasos = "  ---- Pasos para utilizar el programa ---- \n1. Tener PC"
+
+def mensajeBienvenida():
+    print(descripcion)
+    print(fecha)
+    print(autor)
+    print(licencia)
+    print(pasos)
+
+mensajeBienvenida()
+mensajeBienvenida()
+mensajeBienvenida()
+
+
